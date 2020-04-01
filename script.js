e1bead36c1aa3dfb7dd11123eee77fa8909059b3
Anton <antonkobelev85@mail.ru>
2020-04-01 22:08:26 +0500

    add function chooseExpenses, checkSavings

--- script.js
+++ script.js
@@ -1,15 +1,13 @@
-let money, time, appData, mandatoryExpenditureItem1, cost1, mandatoryExpenditureItem2, cost2, budgetFor1Day;
+let money, time, appData, mandatoryExpenditureItem, cost, budgetFor1Day;
 
 function start () {  
-    time = prompt("Введите дату в формате YYYY-MM-DD");
     //isNaN проверка money - возвращает true если в переменную записываются не цифры, цикл будет работать дальше
     while(isNaN(money) || money == "" || money == null) {
         money = +prompt("Ваш бюджет на месяц?", '');
+        time = prompt("Введите дату в формате YYYY-MM-DD");
     }
 }
-
 start();
-
 
 appData = {
     budget:money, 
@@ -17,30 +15,24 @@ appData = {
     expenses : {}, 
     optionalExpenses: {}, 
     income : [], 
-    savings: false};//создаем объект для хранения данных
+    savings: true};//создаем объект для хранения данных
 
-
-mandatoryExpenditureItem1 = prompt("Введите обязательную статью расходов в этом месяце");
-cost1 = prompt("Во сколько обойдется?");
-mandatoryExpenditureItem2 = prompt("Введите обязательную статью расходов в этом месяце");
-cost2 = prompt("Во сколько обойдется?");
-appData.expenses[mandatoryExpenditureItem1] = cost1;//добавляем в объект expenses пару ключ-значение обяз.статья расходов - стоимость
-appData.expenses[mandatoryExpenditureItem2] = cost2;//добавляем в объект expenses пару ключ-значение обяз.статья расходов - стоимость
-budgetFor1Day = (appData.moneyKey)/30; //из бюджета на месяц вычтем обяз.статью расходов и разделим на 30
-alert(budgetFor1Day);
-
-for (let i = 0; i < 2; i++) {
-    mandatoryExpenditureItem = prompt("Введите обязательную статью расходов в этом месяце");
-    cost = prompt("Во сколько обойдется?");
-    if (typeof (mandatoryExpenditureItem === 'string') && mandatoryExpenditureItem != null && typeof cost != null && mandatoryExpenditureItem != '' && cost != '' && mandatoryExpenditureItem.length < 50) { // проверяем явл. ли mandatoryExpenditureItem строкой и не является null и cost не является null и mandatoryExpenditureItem cost  не является пустой строкой и длина mandatoryExpenditureItem должна быть меньше 50
-        console.log('done');
-        appData.expenses[mandatoryExpenditureItem] = cost; //добавляем в объект expenses пару ключ-значение обяз.статья расходов - стоимость
-    } else {
-        i--; //если if не срабатывает то компенсируем i, то есть обнуляем чтобы еще раз if сработал
+function chooseExpenses () {
+    for (let i = 0; i < 2; i++) {
+        mandatoryExpenditureItem = prompt("Введите обязательную статью расходов в этом месяце");
+        cost = prompt("Во сколько обойдется?");
+        if (typeof (mandatoryExpenditureItem === 'string') && mandatoryExpenditureItem != null && typeof cost != null && mandatoryExpenditureItem != '' && cost != '' && mandatoryExpenditureItem.length < 50) { // проверяем явл. ли mandatoryExpenditureItem строкой и не является null и cost не является null и mandatoryExpenditureItem cost  не является пустой строкой и длина mandatoryExpenditureItem должна быть меньше 50
+            console.log('done');
+            appData.expenses[mandatoryExpenditureItem] = cost; //добавляем в объект expenses пару ключ-значение обяз.статья расходов - стоимость
+        } else {
+            i--; //если if не срабатывает то компенсируем i, то есть обнуляем чтобы еще раз if сработал
+        }
     }
 }
 
-appData.budgetFor1Day = (appData.moneyKey) / 30; // разделим на 30 бюджет и добавим в объект appData новое свойство budgetFor1Day
+chooseExpenses();
+
+appData.budgetFor1Day = (appData.budget / 30).toFixed(); // разделим на 30 бюджет и добавим в объект appData новое свойство budgetFor1Day и округлим
 
 alert("Ежедневный бюджет " + appData.budgetFor1Day);
 
@@ -53,3 +45,15 @@ if (appData.budgetFor1Day < 100) {
 } else {
     console.log("Произошла ошибка");
 }
+
+//функция для расчета накопления с депозита
+function checkSavings() {
+    if (appData.savings == true) { //проверяем есть ли сбережения?
+        let save = +prompt("Какова сумма накоплений?"),
+            persent = +prompt("Под какой процент?");
+        appData.monthIncome = save/100/12*persent; //добавляем в объект , создаем новое свойство monthIncome. Переводим в проценты - делим на 100 и делим на 12 месяцев и умножаем на процент
+        alert("Доход в месяц с вашего депозита: " + appData.monthIncome);
+    }
+}
+
+checkSavings();
